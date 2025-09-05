c1d0f2c8c254b30f41e2c369794ee7477b6a6e8d
MrBitasmblTester2 <lulu2214@proton.me>
2025-09-05 16:42:28 +0400

    Add heatmap.component.ts

---
+++ src/app/heatmap/heatmap.component.ts
@@ -0,0 +1,6 @@
+// import { Component } from '@angular/core';
+@Component({selector:'app-heatmap',templateUrl:'./heatmap.component.html'})
+export class HeatmapComponent {
+  data = []; // load via MoodService
+  // map each entry to a color
+}
